a1e20b17884882a0c3058d2a3ca3f3047b107eb9
Matt Seath <seath.dev@gmail.com>
2023-03-26 18:35:53 +1000

    pretty urls are back

--- public/index.search.js
+++ public/index.search.js
@@ -4,48 +4,48 @@ var relearn_search_index = [
     "description": "",
     "tags": null,
     "title": "Articles",
-    "uri": "/articles.html"
+    "uri": "/articles/index.html"
   },
   {
-    "content": " graph TB; A[Hard edge] --\u003e|Link text| B(Round edge) B --\u003e C{\u003cstrong\u003eDecision\u003c/strong\u003e} C --\u003e|One| D[Result one] C --\u003e|Two| E((j kkljlkjlkjlk)) click E \"/cloudlogic/authentication-issues.html\" click B \"/cloudlogic/authentication-issues.html\" ",
+    "content": " graph TB; A[Hard edge] --\u003e|Link text| B(Round edge) B --\u003e C{\u003cstrong\u003eDecision\u003c/strong\u003e} C --\u003e|One| D[Result one] C --\u003e|Two| E((j kkljlkjlkjlk)) click E \"/cloudlogic/authentication-issues/\" click B \"/cloudlogic/authentication-issues/\" ",
     "description": "",
     "tags": null,
     "title": "Example",
-    "uri": "/articles/example.html"
+    "uri": "/articles/example/index.html"
   },
   {
     "content": "",
     "description": "",
     "tags": null,
     "title": "Authentication Issues",
-    "uri": "/cloudlogic/authentication-issues.html"
+    "uri": "/cloudlogic/authentication-issues/index.html"
   },
   {
-    "content": " graph TB; A[Hard edge] --\u003e|Link text| B(Round edge) B --\u003e C{\u003cstrong\u003eDecision\u003c/strong\u003e} C --\u003e|One| D[Result one] C --\u003e|Two| E((j kkljlkjlkjlk)) click E \"/cloudlogic/authentication-issues.html\" click B \"/cloudlogic/authentication-issues.html\" ",
+    "content": " graph TB; A[Hard edge] --\u003e|Link text| B(Round edge) B --\u003e C{\u003cstrong\u003eDecision\u003c/strong\u003e} C --\u003e|One| D[Result one] C --\u003e|Two| E((j kkljlkjlkjlk)) click E \"/cloudlogic/authentication-issues/\" click B \"/cloudlogic/authentication-issues/\" ",
     "description": "",
     "tags": null,
     "title": "CloudLogic",
-    "uri": "/cloudlogic.html"
+    "uri": "/cloudlogic/index.html"
   },
   {
     "content": "",
     "description": "",
     "tags": null,
     "title": "DAIS",
-    "uri": "/dais.html"
+    "uri": "/dais/index.html"
   },
   {
     "content": "",
     "description": "",
     "tags": null,
     "title": "Categories",
-    "uri": "/categories.html"
+    "uri": "/categories/index.html"
   },
   {
     "content": "",
     "description": "",
     "tags": null,
     "title": "Tags",
-    "uri": "/tags.html"
+    "uri": "/tags/index.html"
   }
 ]
